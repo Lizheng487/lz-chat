750ad6f45b25517f46e2b91085f19c0fb8ca1a5d
Lizheng487 <2741372576@qq.com>
2025-12-22 10:13:02 +0800

    feat: 项目初始化，项目扁平化，i18n

---
+++ renderer/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { NConfigProvider } from 'naive-ui';
+
+</script>
+<template>
+  <n-config-provider class="h-[100vh] w-[100vw] flex">
+    <aside class=" h-full flex flex-shrink-0 flex-col bg-amber-600 w-[200px]">
+      Aside
+    </aside>
+    <div class="flex-auto">
+      <title-bar>
+        <drag-region class="w-full" />
+      </title-bar>
+      Main
+    </div>
+  </n-config-provider>
+</template>
---
+++ vite.renderer.config.ts
@@ -0,0 +1,42 @@
+import { type CSSOptions, defineConfig } from "vite";
+import { resolve } from "node:path";
+
+// https://vitejs.dev/config
+export default defineConfig(async () => {
+  const vue = (await import("@vitejs/plugin-vue")).default;
+  const tailwindcss = (await import("@tailwindcss/vite")).default;
+  const autoImport = (await import("unplugin-auto-import/vite")).default;
+
+  return {
+    plugins: [
+      vue(),
+      tailwindcss(),
+      autoImport({
+        imports: ["vue", "vue-router", "pinia", "vue-i18n", "@vueuse/core"],
+        dts: "renderer/auto-import.d.ts",
+      }),
+    ],
+    css: {
+      transformer: "lightningcss" as CSSOptions["transformer"],
+    },
+    build: {
+      target: "es2022",
+      publicDir: "public",
+      rollupOptions: {
+        input: [
+          resolve(__dirname, "html/index.html"),
+          resolve(__dirname, "html/dialog.html"),
+          resolve(__dirname, "html/setting.html"),
+        ],
+      },
+    },
+    resolve: {
+      alias: {
+        "@common": resolve(__dirname, "common"),
+        "@renderer": resolve(__dirname, "renderer"),
+        "@main": resolve(__dirname, "main"),
+        "@locales": resolve(__dirname, "locales"),
+      },
+    },
+  };
+});
