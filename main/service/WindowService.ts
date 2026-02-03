7cc8ea7dda363a962082f05d5253464f7e632c39
Lizheng487 <2741372576@qq.com>
2026-02-03 17:24:15 +0800

    feat: 新增多窗口的适配和loading动画

--- main/service/WindowService.ts
+++ main/service/WindowService.ts
@@ -4,15 +4,16 @@ import {
   ipcMain,
   type IpcMainEvent,
   IpcMainInvokeEvent,
+  WebContentsView,
 } from "electron";
 import path from "node:path";
 import type { WindowNames } from "@common/types";
 import { debounce } from "@common/utils";
-import { IPC_EVENTS } from "@common/constants";
+import { IPC_EVENTS, WINDOW_NAMES } from "@common/constants";
 import logManager from "./LogService";
 import themeManager from "./ThemeService";
 
-interface SizeOption {
+interface SizeOptions {
   width: number;
   height: number;
   minWidth?: number;
@@ -21,6 +22,8 @@ interface SizeOption {
   maxHeight?: number;
 }
 const SHARED_WINDOW_OPTIONS = {
+  opacity: 0,
+  show: false,
   titleBarStyle: "hidden",
   title: "LzChat",
   darkTheme: themeManager.isDark,
@@ -44,14 +47,23 @@ class WindowService {
   private static _instance: WindowService;
   private _winStates: Record<WindowNames | string, WindowState> = {
     main: { instance: void 0, isHidden: false, onCreate: [], onClosed: [] },
+    setting: { instance: void 0, isHidden: false, onCreate: [], onClosed: [] },
+    dialog: { instance: void 0, isHidden: false, onCreate: [], onClosed: [] },
   };
   private constructor() {
     this._setupIpcEvents();
     logManager.info("WindowService initialized successfully.");
   }
+  private _isReallyClose(windowName: WindowNames | void) {
+    if (windowName === WINDOW_NAMES.MAIN) return true;
+    if (windowName === WINDOW_NAMES.SETTING) return false;
+    return true;
+  }
   private _setupIpcEvents() {
     const handleCloseWindow = (e: IpcMainEvent) => {
-      this.close(BrowserWindow.fromWebContents(e.sender));
+      const target = BrowserWindow.fromWebContents(e.sender);
+      const winName = this.getName(target);
+      this.close(target, this._isReallyClose(winName));
     };
     const handleMinimizeWindow = (e: IpcMainEvent) => {
       BrowserWindow.fromWebContents(e.sender)?.minimize();
@@ -73,14 +85,105 @@ class WindowService {
     }
     return this._instance;
   }
-  public create(name: WindowNames, size: SizeOption) {
-    const window = new BrowserWindow({
-      ...SHARED_WINDOW_OPTIONS,
-      ...size,
+  public create(
+    name: WindowNames,
+    size: SizeOptions,
+    moreOpts?: BrowserWindowConstructorOptions
+  ) {
+    if (this.get(name)) return;
+    const isHiddenWin = this._isHiddenWin(name);
+    let window = this._createWinInstance(name, moreOpts);
+    !isHiddenWin &&
+      this._setupWinLifecycle(window, name)._loadWindowTemplate(window, name);
+    this._listenWinReady({
+      win: window,
+      isHiddenWin,
+      size,
     });
-    this._setupWinLifecycle(window, name)._loadWindowTemplate(window, name);
-    this._winStates[name].onCreate.forEach((callback) => callback(window));
+    if (!isHiddenWin) {
+      this._winStates[name].instance = window;
+      this._winStates[name].onCreate.forEach((callback) => callback(window));
+    }
+    if (isHiddenWin) {
+      this._winStates[name].isHidden = false;
+      logManager.info(`Hidden Window show: ${name}`);
+    }
     return window;
+  }
+  private _listenWinReady(params: {
+    win: BrowserWindow;
+    isHiddenWin: boolean;
+    size: SizeOptions;
+  }) {
+    const onReady = () => {
+      params.win?.once("show", () =>
+        setTimeout(() => this._applySizeConstraints(params.win, params.size), 2)
+      );
+      params.win?.show();
+    };
+    if (!params.isHiddenWin) {
+      const loadingHandler = this._addLoadingView(params.win, params.size);
+      loadingHandler?.(onReady);
+    } else {
+      onReady();
+    }
+  }
+  private _addLoadingView(window: BrowserWindow, size: SizeOptions) {
+    let loadingView: WebContentsView | void = new WebContentsView();
+    let rendererIsReady = false;
+    window.contentView?.addChildView(loadingView);
+    loadingView.setBounds({
+      x: 0,
+      y: 0,
+      width: size.width,
+      height: size.height,
+    });
+    loadingView.webContents.loadFile(path.join(__dirname, "loading.html"));
+    const onRendererIsReady = (e: IpcMainEvent) => {
+      if (e.sender !== window?.webContents || rendererIsReady) return;
+      rendererIsReady = true;
+      window.contentView.removeChildView(loadingView as WebContentsView);
+      ipcMain.removeListener(IPC_EVENTS.RENDERER_IS_READY, onRendererIsReady);
+      loadingView = void 0;
+    };
+    ipcMain.on(IPC_EVENTS.RENDERER_IS_READY, onRendererIsReady);
+    return (cb: () => void) =>
+      loadingView?.webContents.once("dom-ready", () => {
+        loadingView?.webContents.insertCSS(`body {
+          background-color: ${
+            themeManager.isDark ? "#2C2C2C" : "#FFFFFF"
+          } !important; 
+          --stop-color-start: ${
+            themeManager.isDark ? "#A0A0A0" : "#7F7F7F"
+          } !important;
+          --stop-color-end: ${
+            themeManager.isDark ? "#A0A0A0" : "#7F7F7F"
+          } !important;
+      }`);
+        cb();
+      });
+  }
+  private _applySizeConstraints(win: BrowserWindow, size: SizeOptions) {
+    if (size.maxHeight && size.maxWidth) {
+      win.setMaximumSize(size.maxWidth, size.maxHeight);
+    }
+    if (size.minHeight && size.minWidth) {
+      win.setMinimumSize(size.minWidth, size.minHeight);
+    }
+  }
+  private _isHiddenWin(name: WindowNames) {
+    return this._winStates[name] && this._winStates[name].isHidden;
+  }
+  private _createWinInstance(
+    name: WindowNames,
+    opts?: BrowserWindowConstructorOptions
+  ) {
+    return this._isHiddenWin(name)
+      ? (this._winStates[name].instance as BrowserWindow)
+      : new BrowserWindow({
+          ...SHARED_WINDOW_OPTIONS,
+          ...opts,
+        });
   }
   private _setupWinLifecycle(window: BrowserWindow, name: WindowNames) {
     const updateWinStatus = debounce(
@@ -96,6 +199,8 @@ class WindowService {
       this._winStates[name].onClosed.forEach((callback) => callback(window));
       window?.destroy();
       window?.removeListener("resize", updateWinStatus);
+      this._winStates[name].instance = void 0;
+      this._winStates[name].isHidden = false;
       logManager.info(`Window closed: ${name}`);
     });
     window.on("resize", updateWinStatus);
@@ -118,9 +223,56 @@ class WindowService {
       )
     );
   }
-  public close(target: BrowserWindow | void | null) {
+  public getName(target: BrowserWindow | void | null): WindowNames | void {
     if (!target) return;
-    target?.close();
+    for (const [name, win] of Object.entries(this._winStates) as [
+      WindowNames,
+      { instance: BrowserWindow | void } | void
+    ][]) {
+      if (win?.instance === target) return name;
+    }
+  }
+  public get(name: WindowNames) {
+    if (this._winStates[name].isHidden) {
+      return void 0;
+    }
+    return this._winStates[name].instance;
+  }
+  private _handleCloseWindowState(target: BrowserWindow, really: boolean) {
+    const name = this.getName(target) as WindowNames;
+    if (name) {
+      if (!really) {
+        this._winStates[name].isHidden = true;
+      } else {
+        this._winStates[name].instance = void 0;
+      }
+    }
+    setTimeout(() => {
+      target[really ? "close" : "hide"]?.();
+      this._checkAndCloseAllWindows();
+    }, 300);
+  }
+  private _checkAndCloseAllWindows() {
+    if (
+      !this._winStates[WINDOW_NAMES.MAIN].instance ||
+      this._winStates[WINDOW_NAMES.MAIN].instance?.isDestroyed()
+    ) {
+      return Object.values(this._winStates).forEach((win) =>
+        win?.instance?.close?.()
+      );
+    }
+    const minimizeToTray = false;
+    if (!minimizeToTray && !this.get(WINDOW_NAMES.MAIN)?.isVisible()) {
+      return Object.values(this._winStates).forEach(
+        (win) => !win?.instance?.isVisible() && win?.instance?.close?.()
+      );
+    }
+  }
+  public close(target: BrowserWindow | void | null, really: boolean = true) {
+    if (!target) return;
+    const name = this.getName(target);
+    logManager.info(`Closing window: ${name},really: ${really}`);
+    this._handleCloseWindowState(target, really);
   }
   public toggleMax(target: BrowserWindow | void | null) {
     if (!target) return;
